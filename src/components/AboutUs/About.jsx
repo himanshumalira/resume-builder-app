d692ff413fe2f53eccdd36c0ea02d71ff7715eed
himanshumalira <147631977+himanshumalira@users.noreply.github.com>
2024-04-07 17:27:15 +0530

    about change

--- src/components/AboutUs/About.jsx
+++ src/components/AboutUs/About.jsx
@@ -1,32 +1,67 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-      <div className="py-16 bg-white">
-          <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-              <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
-                  <div className="md:5/12 lg:w-5/12">
-                      <img
-                          src="https://images.pexels.com/photos/20480902/pexels-photo-20480902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                          alt="image"
-                      />
-                  </div>
-                  <div className="md:7/12 lg:w-6/12">
-                      <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
-                          Himanshu Pathak : A Full Stack Web Developer
-                      </h2>
-                      <p className="mt-6 text-gray-600">
-                          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
-                          accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
-                          aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
-                      </p>
-                      <p className="mt-4 text-gray-600">
-                          Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
-                          Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
-                      </p>
-                  </div>
-              </div>
+    <div className="py-16 bg-white">
+      <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+        <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+          <div className="md:5/12 lg:w-5/12">
+            <img
+              src="https://images.pexels.com/photos/20480902/pexels-photo-20480902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt="image"
+            />
           </div>
+          <div className="md:7/12 lg:w-6/12">
+            <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+              Himanshu Pathak : A Full Stack Web Developer
+            </h2>
+            <p className="mt-6 text-gray-600">
+              My name is Himanshu Pathak, and I am a dedicated Full Stack and
+              Front-End Developer based in Muzaffarnagar, Uttar Pradesh. After
+              coming across the Front-End Developer position with your
+              organization, I was instantly attracted to the opportunity, and I
+              believe that my unique blend of skills, passion, and experience
+              would bring substantial value to your team. Navigating through
+              novel opportunities and challenges as a Quality Engineer at
+              Xpression Graphic And Print Pvt Ltd (March 2021 - May 2023), I was
+              able to flex my technical muscles, demonstrate my adaptability and
+              refine my problem-solving capabilities. This experience helped me
+              to appreciate the fact that quality is not an act but a habit,
+              which is imperatively reflected in every line of code I write. I
+              hold a Bachelor of Technology from the renowned Dr. APJ Abdul
+              Kalam Technical University, which has provided a solid foundation
+              for my technological prowess. Meticulously honing my HTML5, CSS,
+              Node.js, Reactjs, Bootstrap, Tailwind CSS, Javascript, Reach
+              Routers, React Redux, and Redux Toolkit skills, I have developed
+              an adaptive understanding of these technologies (all at 100%
+              proficiency) which serve as powerful armour while attacking
+              complex front-end challenges. My passion for web and front-end
+              development is not just limited to the workplace. My portfolio
+              (https://himanshumaIira.github.io/midassignment/) carries the
+              thumbprints of my love for this work. It showcases a range of
+              personal projects, including a personal portfolio website, a blog
+              web app, and a Todo web app. I believe that these not only speak
+              for my technical abilities, but also prove my dedication to
+              superior creativity, user- focused design, and pragmatic solution
+              implementation. Aside from my technical expertise and experience,
+              I am a firm believer in aligning myself with organizations that
+              recognize the true indispensability of technological innovation. I
+              am particularly drawn to your company's commitment to dynamic web
+              solutions and would love to be part of an institution that
+              appreciates innovative technology as much as I do. I eagerly look
+              forward to the possibility of discussing my application with you
+              further. Please feel free to call (8445901273) or email
+              (himanshumalira@gmail.com) at your convenience. Thank you for
+              considering my application. Sincerely, Himanshu Pathak
+            </p>
+            <p className="mt-4 text-gray-600">
+              Nobis minus voluptatibus pariatur dignissimos libero quaerat iure
+              expedita at? Asperiores nemo possimus nesciunt dicta veniam
+              aspernatur quam mollitia.
+            </p>
+          </div>
+        </div>
       </div>
+    </div>
   );
 }
